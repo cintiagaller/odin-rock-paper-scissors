bf000761bc11564e967d5d85965770fee43ece6f
cintiagaller <cintia.galler@gmail.com>
2023-07-15 14:46:08 +0200

    Add a way to get the player's choice

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-const options = ['Rock', 'Paper', 'Scissors'];
+const options = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
     const randomChoice = Math.floor(Math.random() * options.length);
     return options[randomChoice];
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+    let choice = prompt("Choice of weapon?");
+    choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
 
+    if (!options.includes(choice)) {
+        choice = getComputerChoice();
+    }
+
+    return choice;
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+
+console.log(playerSelection, computerSelection);
